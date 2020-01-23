66cc82e0cf71720b1af0f50ec3958fb2ff812bf4
root <root@localhost.localdomain>
2020-01-23 09:41:24 -0500

    Updated battlestation script

--- BattleStationConfig.sh
+++ BattleStationConfig.sh
@@ -1,6 +1,5 @@
-apt-get install zsh curl git
+apt-get install zsh curl git fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mv vimconfig ~/.vimrc
 mv zshrc ~/.zshrc
-
-echo "Please logout and log back in.\n"
+echo "Vim plugins will be installed on next run. Please logout and log back in.\n"
