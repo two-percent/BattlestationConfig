785d25b7940736235d817230864667dc1d69549d
Two-Percent <milk@two-percent.com>
2022-06-18 18:46:18 -0400

    Getting this project back online. Revamping baseline configurations.

--- BattleStationConfig.sh
+++ BattleStationConfig.sh
@@ -1,5 +1,42 @@
-apt-get install zsh curl git fonts-powerline
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv vimconfig ~/.vimrc
-mv zshrc ~/.zshrc
-echo "Vim plugins will be installed on next run. Please logout and log back in.\n"
+#!/bin/bash
+echo "Running this script will reboot your machine."
+read -p "Continue? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	exit 1
+fi
+
+echo "Installing Sublime Text..."
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
+
+echo "Installing Oh-My-Zsh..."
+
+sudo apt-get install zsh curl git fonts-powerline
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+# install for user and root
+sh install.sh --unattended
+sudo sh install.sh --unattended
+rm install.sh
+
+echo "Setting up logging..."
+sudo mkdir /root/logs
+mkdir ~/logs
+cat BashMod.txt >> ~/.bashrc
+sudo cat Bashmod.txt >> /root/.bashrc
+cat ZshMod.txt >> ~/.zshrc
+sudo cat ZshMod.txt >> /root/.zshrc
+sed -i 's/^ZSH_THEME.*/ZSH_THEME="agnoster"/' ~/.zshrc
+sudo sed -i 's/^ZSH_THEME.*/ZSH_THEME="agnoster"/' /root/.zshrc
+chsh -s /bin/bash
+sudo chsh -s /bin/bash
+
+echo "Updating system..."
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade
+
+echo "Rebooting because why not..."
+sudo reboot
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Setup scripts for personal battlestation preferences
 
 2020-01-23 : Vim config added. Genereated by Vim Bootstrap.
              Started installation script and added zshrc config
+2022-06-18 : Complete overhaul. https://www.youtube.com/watch?v=araU0fZj6oQ
